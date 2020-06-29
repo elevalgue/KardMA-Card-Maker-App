a06abb84be253816a4359186dad7228d403058ac
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-29 17:06:26 +0200

    add update form preview

--- src/js/03-form-preview.js
+++ src/js/03-form-preview.js
@@ -1,34 +1,51 @@
-//FUNCIONES PARA RELLENAR FULLNAME
-function valueInputName() {
-    return (infoPerson.name = fullName.value);
+// FUNCION PARA RELLENAR PREVIEW -- NAME
+function handleUpdateFullName() {
+  const fullNameFilled = fullName.value;
+  if (fullName.value === "") {
+    previewFullName.innerHTML = "Nombre Apellido";
+  } else {
+    previewFullName.innerHTML = fullNameFilled;
   }
-  function resultFullName(fullNameFilled) {
-    if (fullName.value === "") {
-      previewFullName.innerHTML = "Nombre Apellido";
-    } else {
-      previewFullName.innerHTML = fullNameFilled;
-    }
+}
+
+// FUNCION PARA RELLENAR PREVIEW -- JOB
+function handleUpdateJob() {
+  const jobFilled = job.value;
+  if (job.value === "") {
+    previewJob.innerHTML = "Front-end developer";
+  } else {
+    previewJob.innerHTML = jobFilled;
   }
-  function handleInputFullName() {
-    const fullNameFilled = valueInputName();
-    resultFullName(fullNameFilled);
-  }
-  //FUNCIONES PARA RELLENAR JOB
-  function valueInputJob() {
-    return (infoPerson.job = job.value);
-  }
-  function resultJob(jobFilled) {
-    if (job.value === "") {
-      previewJob.innerHTML = "Front-end developer";
-      console.log("errror");
-    } else {
-      previewJob.innerHTML = jobFilled;
-    }
-  }
-  function handleInputJob() {
-    const jobFilled = valueInputJob();
-    resultJob(jobFilled);
-  }
-  //LISTENERS -- copiado para poder usarlo
-  fullName.addEventListener("keyup", handleInputFullName);
-  job.addEventListener("keyup", handleInputJob);
+}
+
+// FUNCION PARA RELLENAR PREVIEW -- EMAIL
+function handleUpdateEmail() {
+  const emailFilled = email.value;
+  return (previewEmail.href = "mailto:" + emailFilled);
+}
+
+// FUNCION PARA RELLENAR PREVIEW -- TELEFONO
+function handleUpdateTelephone() {
+  const telephoneFilled = telephone.value;
+  return (previewTelephone.href = "tel:" + telephoneFilled);
+}
+
+// FUNCION PARA RELLENAR PREVIEW -- LINKEDIN
+function handleUpdateLinkedIn() {
+  const linkedInFilled = linkedIn.value;
+  return (previewLinkedIn.href = linkedInFilled);
+}
+
+// FUNCION PARA RELLENAR PREVIEW -- GITHUB
+function handleUpdateGitHub() {
+  const gitHubFilled = gitHub.value;
+  return (previewGitHub.href = gitHubFilled);
+}
+
+// LISTENERS
+fullName.addEventListener("keyup", handleUpdateFullName);
+job.addEventListener("keyup", handleUpdateJob);
+email.addEventListener("keyup", handleUpdateEmail);
+telephone.addEventListener("keyup", handleUpdateTelephone);
+linkedIn.addEventListener("keyup", handleUpdateLinkedIn);
+gitHub.addEventListener("keyup", handleUpdateGitHub);
