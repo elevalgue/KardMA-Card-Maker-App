135573b4d91310746d6ebee714f36968fc073a1f
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-06-29 18:51:28 +0200

    add arrow & toggle js

--- src/js/02-collapsible.js
+++ src/js/02-collapsible.js
@@ -1,5 +1,19 @@
-"use strict";
+//CERRAR DESIGN SECTION
+function hideDesign() {
+  formOpenDesign.classList.toggle('hidden');
+  divArrowDesign.classList.toggle('closed');
+}
+formCloseDesign.addEventListener('click', hideDesign);
 
-// toggle
-
-const collapsibleForm = document.querySelectorAll(".");
+//CERRAR FILL SECTION
+function hideFill() {
+  formOpenFill.classList.toggle('hidden');
+  divArrowFill.classList.toggle('closed');
+}
+formCloseFill.addEventListener('click', hideFill);
+//CERRAR SHARE SECTION
+function hideShare() {
+  formOpenShare.classList.toggle('hidden');
+  divArrowShare.classList.toggle('closed');
+}
+formCloseShare.addEventListener('click', hideShare);
