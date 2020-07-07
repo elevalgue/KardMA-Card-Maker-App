6eddbfe01143e69a778fb5fbea50d0028c47874c
amandapalma <amandapalma00@gmail.com>
2020-07-07 12:13:24 +0200

    add card url generator

--- src/js/05-submit.js
+++ src/js/05-submit.js
@@ -1,33 +1,34 @@
+"use strict";
 //LISTENERS
-fullName.addEventListener('keyup', () => {
+fullName.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-job.addEventListener('keyup', () => {
+job.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-selectImagen.addEventListener('change', () => {
+selectImagen.addEventListener("change", () => {
   valueInput;
   validateForm;
 });
-email.addEventListener('keyup', () => {
+email.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-telephone.addEventListener('keyup', () => {
+telephone.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-linkedIn.addEventListener('keyup', () => {
+linkedIn.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-gitHub.addEventListener('keyup', () => {
+gitHub.addEventListener("keyup", () => {
   valueInput();
   validateForm();
 });
-resetButton.addEventListener('click', resetForm);
+resetButton.addEventListener("click", resetForm);
 
 // FUNCIÓN PARA VALIDAR EL FORMULARIO:
 
@@ -40,9 +41,48 @@ function validateForm() {
     infoPerson.linkedin.length < 1 ||
     infoPerson.github.length < 1
   ) {
-    submitButton.setAttribute('disabled', 'disabled');
+    submitButton.setAttribute("disabled", "disabled");
   } else {
-    submitButton.removeAttribute('disabled');
-    submitButton.classList.remove('cursor__btn__submit');
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.remove("cursor__btn__submit");
+  }
+}
+
+submitButton.addEventListener("click", sendData);
+
+// GENERAR URL DE LA TARJETA
+
+const responseURL = document.querySelector(".response");
+
+function sendData(ev) {
+  ev.preventDefault();
+  sendRequest(infoPerson);
+}
+
+function sendRequest(json) {
+  fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
+    method: "POST",
+    body: JSON.stringify(json),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (result) {
+      showURL(result);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function showURL(result) {
+  if (result.success) {
+    responseURL.innerHTML =
+      "<a href=" + result.cardURL + ">" + result.cardURL + "</a>";
+  } else {
+    responseURL.innerHTML = "ERROR:" + result.error;
   }
 }
